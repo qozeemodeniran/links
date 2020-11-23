645743d4475cfd0d7f730ee428331bebac6fc8e6
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2020-11-23 16:01:32 +0100

    done with routing and views

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $links = \App\Models\Link::all();
+
+    return view('welcome', ['links' => $links]);
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
